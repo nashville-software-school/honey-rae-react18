f59aa679cc998e40c909be1668a3c71f3d97ea5a
Hannah Hall <hannahkhall13@gmail.com>
2023-04-23 14:30:05 -0500

    nav

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -5,15 +5,13 @@ import { Authorized } from "./Authorized"
 
 
 export const ApplicationViews = () => {
-    return <>
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route element={<Authorized />}>
-                <Route path="/" element={<TicketList />} />
-                <Route exact path="/tickets/:ticketId" element={<Ticket />} />
-                <Route path="/tickets/create" element={<TicketForm />} />
-            </Route>
-        </Routes>
-    </>
+  return <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route element={<Authorized />}>
+      <Route path="/" element={<TicketList />} />
+      <Route exact path="/tickets/:ticketId" element={<Ticket />} />
+      <Route path="/tickets/create" element={<TicketForm />} />
+    </Route>
+  </Routes>
 }
